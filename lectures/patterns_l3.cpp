f5b23a3b2e238d2ee53b57e5b09401ef03ae11a6
Md Aatif Eqbal <aatiftechnical123@gmail.com>
2026-07-19 09:55:33 +0530

    pattern problems

--- lectures/patterns_l3.cpp
+++ lectures/patterns_l3.cpp
@@ -5,7 +5,29 @@ using namespace std;
 
 int main() {
     
+    // Square pattern
+    int rowsColumns = 5; //both are same bcvause of square
+    for(int i = 1; i<=rowsColumns; i++ ) {
+        for(int j = 0; j <= rowsColumns; j++) {
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    cout<<"\n";
     
+    // right triangle pattern
+    
+    int n = 5; //rows
+    int m = 4; //columns
+    for(int i =1; i <= n; i++) {
+       for(int j = 1; j <= i; j++) {
+           cout<<"*";
+       } 
+       cout<<endl;
+    }
+    
+    // inverse right triangle pattern
+       
     
     return 0;
 }

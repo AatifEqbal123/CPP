e9ef7691e3e0071d88850f14fe2d14a8f6de91dc
Md Aatif Eqbal <aatiftechnical123@gmail.com>
2026-07-19 20:47:00 +0530

    second highest through linear search form an array

--- lectures/patterns_l3.cpp
+++ lectures/patterns_l3.cpp
@@ -1,50 +1,75 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
-int main() {
-    
+int main()
+{
     // Square pattern
     int rowsColumns = 5; //both are same bcvause of square
-    for(int i = 1; i<=rowsColumns; i++ ) {
-        for(int j = 0; j <= rowsColumns; j++) {
-            cout<<"*";
+    for (int i = 1; i <= rowsColumns; i++)
+    {
+        for (int j = 0; j <= rowsColumns; j++)
+        {
+            cout << "*";
         }
-        cout<<"\n";
+        cout << "\n";
     }
-    cout<<"\n";
-    
+    cout << "\n";
+
     // right triangle pattern
-    
+
     int n = 5; //rows
     int m = 4; //columns
-    for(int i =1; i <= n; i++) {
-       for(int j = 1; j <= i; j++) {
-           cout<<"*";
-       } 
-       cout<<endl;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
     }
-    
+
     // inverse right triangle pattern
-    
-    for(int i = 1; i <= n; i++) {
-       for(int j = 5; j >= i; j--) {
-           cout<<"*";
-       }
-       cout<<""<<endl;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 5; j >= i; j--)
+        {
+            cout << "*";
+        }
+        cout << "" << endl;
     }
-    
-    cout<<"\n";
-    
-    // highest of the array
-    int arr[5] = {9,2,7,6,3};
+
+    cout << "\n";
+
+    // highests of the array
+    // through linear search and loops
+    int arr[4] = {6,4,8,3};
     int highest = 0;
-    for(int i : arr) {
-        if(i >= highest) {
+    int secondHigh = 0;
+    for (int i : arr)
+    {
+        if (i >= highest)
+        {
             highest = i;
-        } 
+        }
     }
-    cout<<highest<<endl;
-    return 0;
-}
+    { // second highest
+        for (int j : arr)
+        {
+            if (j < highest && j >= secondHigh)
+            {
+                secondHigh = j;
+            }
+        }
+    }
+
+        cout << highest << endl;
+        cout << secondHigh << endl;
+        
+        // through sorting
+   
+        return 0;
+    }

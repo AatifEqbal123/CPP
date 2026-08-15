1b1733b819b6c645fdc3988bb2cdcb41ea98ef8d
Md Aatif Eqbal <aatiftechnical123@gmail.com>
2026-08-15 11:14:07 +0530

    practice done ✓

--- lectures/functions-II.cpp
+++ lectures/functions-II.cpp
@@ -4,15 +4,15 @@
 using namespace std;
 
 //Paractic3
-bool isPrime(int n) {
-    bool isPrime = true;
-    int count = 0;
-    for(int i = 1; i <= (n/2); i++) {
-        if(n%i == 0) {
+bool isPrime(int n) { 
+    bool isPrime = true; 
+    int count = 0; 
+    for(int i = 1; i <= (n/2); i++) { 
+        if(n%i == 0) { 
             count+=1;
         }
     }
-    if(count > 2) {
+    if(count >= 2) {
         isPrime = false;
     }
     return isPrime;
@@ -20,10 +20,36 @@ bool isPrime(int n) {
 
 // Fabonacci Nth term
 
+int getFabonacci(int n) {
+    // declaring older and newer array
+    int arr[2] = {0, 1}; // old, new    
+    
+    // loop for fabonacci
+    for(int i = 1; i<=(n-2); i++) { 
+        int old = arr[1]; 
+        arr[1] += arr[0]; 
+        arr[0] = old; 
+    }    
+    // returning
+    return arr[1];
+}
 
+// list all primes to 100
 
-
-
+void listAllPrime(int n)
+{
+    for (int i = 2; i <= n; i++)
+    {
+        if (i > 2 and i % 2 == 0)
+        {
+            continue;
+        }
+        else if (isPrime(i) == 1)
+        {
+            cout << i << " , ";
+        }
+    }
+}
 
 int main() {
     
@@ -32,4 +58,9 @@ int main() {
     cout<<"enter n ";
     cin>>n;
     cout<<"Prime : "<<isPrime(n)<<endl;
+    
+    // get nth Fabonacci number    
+    cout<<"nth Fabonacci : "<<getFabonacci(n)<<endl;
+    // listAllPrimes
+    listAllPrime(n);
 }
